12f2a21e9035ad386b46e85d00b6a41e7901e788
Marko Milanovic <marko.milanovic@travelperk.com>
2020-11-16 09:58:41 +0100

    fix scoreboard

--- src/RankCountryComponent.js
+++ src/RankCountryComponent.js
@@ -1,7 +1,6 @@
 import React, {Component} from "react";
 import {getFlagForCountry} from "./images";
 import {countryNameMap} from "./constants";
-import {Droppable} from 'react-beautiful-dnd'
 
 export default class RankCountryComponent extends Component {
 
@@ -17,29 +16,23 @@ export default class RankCountryComponent extends Component {
 
     render() {
         return(
-            <Droppable droppableId={this.props.country}>
-                {(provided) => (<div
-                    {...provided.droppableProps}
-                    className={this.props.currentRank === 1 ? "country country--3" : this.props.gotLastVote ? "country country--2" : this.props.inCurrentVotes ? "country country--1" : "country"}>
-                    {this.flagOrRanking()}
-                    <span className={"country__flag"}>
-                        <img src={getFlagForCountry(this.props.country)}/>
-                    </span>
-                    <span className={"country__name"}>
-                        {countryNameMap[this.props.country.toLowerCase()]}
-                    </span>
-                    <span className={"country__rank"}>
-                        {this.props.rank}
-                    </span>
-                    <span className={"country__average"}>
-                        {
-                            this.props.use12P ? this.props.twelvePointRank : this.props.averageRank.toFixed(2)
-                        }
-                    </span>
-                    {provided.placeholder}
-                </div>)
-                }
-            </Droppable>
+            <div className={this.props.currentRank == 1 ? "country country--3" : this.props.gotLastVote ? "country country--2" : this.props.inCurrentVotes ? "country country--1" : "country"}>
+                { this.flagOrRanking() }
+                <span className={"country__flag"}>
+                    <img src={getFlagForCountry(this.props.country)}/>
+                </span>
+                <span className={"country__name"}>
+                    {countryNameMap[this.props.country.toLowerCase()]}
+                </span>
+                <span className={"country__rank"}>
+                    {this.props.rank}
+                </span>
+                <span className={"country__average"}>
+                    {
+                        this.props.use12P ? this.props.twelvePointRank : this.props.averageRank.toFixed(2)
+                    }
+                </span>
+            </div>
         )
     }
 }
